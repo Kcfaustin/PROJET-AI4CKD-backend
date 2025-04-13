c3438535160819ed1f307cc0b1477ce842bd147d
Faustin AGOHOUNDJE <faustinagohoundje@gmail.com>
2025-04-13 10:07:28 +0100

    corps update

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://192.168.1.104:3000'],
+    'allowed_origins' => ['https://nephrosuivi.netlify.app', 'https://projet-front-end.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['*'],
